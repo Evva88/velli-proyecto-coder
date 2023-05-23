81556b53446c1b3433593979431b99895a2fde7b
unknown <evavelli5@gmail.com>
2023-05-23 07:31:33 -0300

    arreglos de nav

--- src/Componentes/Header.jsx
+++ src/Componentes/Header.jsx
@@ -2,7 +2,11 @@ const Header = () =>{
     return(
         <div className="header">
            <div className="container text-center">
-           <img src={"./img/fonto1.jpg"} className="App-logo" alt={"logo"}/>
+           <div className="row">
+           <div className="col">
+           <img src={"./img/fonto1.jpg"} className="App-logo img-fluid" alt={"logo"}/>
+           </div>
+           </div>
            </div>
         </div>
     )
--- src/Componentes/NavBar.jsx
+++ src/Componentes/NavBar.jsx
@@ -2,11 +2,15 @@ import CartWidget from "./CartWidget";
 
 const NavBar = () => {
     return (
-        <div>
+        <div className="navBar">
+          <div className="container">
+          <div className="row">
+          <div className="col">
           <nav className="navbar navbar-expand-lg bg-body-tertiary bg-body-tertiary bg-dark">
           <div className="container-fluid">
           <a className="navbar-brand" href="#">Productos</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <img src={"img/menu(3).svg"} alt="" />
           <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
@@ -20,12 +24,17 @@ const NavBar = () => {
           </ul>
           <form className="d-flex" role="search">
           <input className="form-control me-2" type="search" placeholder="Buscar" aria-label="Buscar"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
+          <button className="btn btn-outline-success" type="submit">Buscar</button>
           </form>
           <div><CartWidget/></div>
-    </div>
-  </div>
-</nav>
+          </div>
+          </div>
+          
+          </nav>
+          </div>
+          </div>
+          </div>
+
         </div>
     )
 }
