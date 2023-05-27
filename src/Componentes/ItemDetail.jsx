bb2face07dc58d84abe38bc495769b787ef4b38b
unknown <evavelli5@gmail.com>
2023-05-27 04:42:14 -0300

    arreglos

--- src/Componentes/ItemDetail.jsx
+++ src/Componentes/ItemDetail.jsx
@@ -1,25 +1,24 @@
 import React, { useState, useEffect } from "react";
 import ItemCount from "./ItemCount";
-import { Link } from "react-router-dom";
 
 const ItemDetail = ({ producto }) => {
   const [item, setItem] = useState({});
 
   useEffect(() => {
     setItem(producto);
+    console.log(producto);
   }, [producto]);
 
   return (
-    <div className="container my-5">
+    <div className="container">
       <div className="col-md-4 my-3">
-             <div className="card" style={{ width: "20rem" }}>
-                <Link to={"/item/" + item.id} className="text-dark text-decoration-none">
-                <img src={item.img} className="card-img-top2" alt={item.nombre} />
+             <div className="card" style={{ width: "25rem" }}>
+                <img src={item.img} alt={item.nombre} className="card-img-top"  />
                 <div className="card-body">
                 <h3>{item.nombre}</h3>
                 <p className="card-text"><span className="text-secondary">{item.nombre}</span><br /><b>${item.precio}</b></p>
                 </div>
-                </Link>
+                
                 <ItemCount stock={item.stock} />
           </div>
             
