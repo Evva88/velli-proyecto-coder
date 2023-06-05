3162d724947a28cca8407ff7fddea7033be02a7d
eva <evavelli5@gmail.com>
2023-06-05 10:14:06 -0300

    prettier

--- src/Componentes/CardImg.jsx
+++ src/Componentes/CardImg.jsx
@@ -1,45 +1,42 @@
 const CardImg = () => {
-    return (
-      <div>
-        <div className="container text-center">
+  return (
+    <div>
+      <div className="container text-center">
         <div className="row align-items-start">
-        <div className="col">
-          <div className="card" style={{ width: "18rem" }}>
-          <img src="./img/chibi.jpg" className="card-img-top" alt="..." />
-          </div>
+          <div className="col">
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="./img/chibi.jpg" className="card-img-top" alt="..." />
+            </div>
           </div>
           <div className="col">
-          <div className="card" style={{ width: "18rem" }}>
-          <img src="./img/pluton.jpg" className="card-img-top" alt="..." />
-          </div>
-          </div>
-          <div className="col">
-          <div className="card" style={{ width: "18rem" }}>
-          <img src="./img/marte.jpg" className="card-img-top" alt="..." />
-          </div>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="./img/pluton.jpg" className="card-img-top" alt="..." />
+            </div>
           </div>
           <div className="col">
-          <div className="card" style={{ width: "18rem" }}>
-          <img src="./img/saturno.jpg" className="card-img-top" alt="..." />
-          </div>
-          </div>
-          <div className="col">
-          <div className="card" style={{ width: "18rem" }}>
-          <img src="./img/neptuno.jpg" className="card-img-top" alt="..." />
-          </div>
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="./img/marte.jpg" className="card-img-top" alt="..." />
+            </div>
           </div>
           <div className="col">
-          <div className="card" style={{ width: "18rem" }}>
-          <img src="./img/venus.jpg" className="card-img-top" alt="..." />
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="./img/saturno.jpg" className="card-img-top" alt="..." />
+            </div>
           </div>
+          <div className="col">
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="./img/neptuno.jpg" className="card-img-top" alt="..." />
+            </div>
           </div>
+          <div className="col">
+            <div className="card" style={{ width: "18rem" }}>
+              <img src="./img/venus.jpg" className="card-img-top" alt="..." />
+            </div>
           </div>
+        </div>
       </div>
-      </div>
-      
-      
-    );
-  }
-  
-  export default CardImg;
-  
+    </div>
+  );
+};
+
+export default CardImg;
